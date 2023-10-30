e6128aa5cf95a282fe7f7943e3d9c62757c5c278
Guyot Florian <florian.guyot@isi.fraunhofer.de>
2023-10-30 15:06:23 +0100

    Created game_screen component

---
+++ front-end/src/app/game-screen/game-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'epf-game-screen',
+  templateUrl: './game-screen.component.html',
+  styleUrls: ['./game-screen.component.scss']
+})
+export class GameScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
